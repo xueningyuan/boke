f0e5170d948a1d44c214f4c1896e9006ca22ebbd
a2298593298 <2298593298@qq.com>
2018-08-29 11:54:31 +0800

    pdo

---
+++ pdo.php
@@ -0,0 +1,47 @@
+<?php
+$host = '127.0.0.1';
+$dbname = 'bock';
+$user = 'root';
+$pass = '123';
+
+$pdo = new PDO("mysql:host={$host};dbname={$dbname}",$user,$pass);
+$pdo->exec('SET NAMES utf8');
+
+$res = $pdo->prepare('insert into blogs(title,content) values(?,?)');
+$res->execute([
+    '标题？',
+    '内容'
+]);
+echo $pdo->lastInsertId();
+
+
+// $res = $pdo->prepare('select * from blogs where id = :id');
+// $data = $res->execute([':id'=>'2']);
+// $data=$res->fetch(PDO::FETCH_ASSOC);
+// var_dump($data);
+
+
+// $res = $pdo->query('select * from blogs limit 10');
+// $data=$res->fetchAll(PDO::FETCH_ASSOC);
+// for ($i=0; $i < count($data); $i++) { 
+//     echo $data[$i]['title'];
+// }
+
+
+
+// for ($i=0; $i < 100; $i++) { 
+//      $title = getChar(rand(20,100)); 
+//      $content = getChar(rand(20,100)); 
+//     $pdo->exec("insert into blogs(title,content) values('{$title}','{$content}')");
+// }
+
+// $res = $pdo->exec("insert into blogs(title,content) values('标题','hehe')");
+// $res = $pdo->exec("update blogs set title='标题2' where id =1");
+// $res = $pdo->exec("delete from blogs");
+// $res = $pdo->exec("truncate blogs");
+// if($res=false){
+//     echo '出错了';
+// }
+
+
+?>
