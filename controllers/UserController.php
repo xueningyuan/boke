6ffe20a703595a1b023ce4d6f9390d7f78e2a19c
a2298593298 <2298593298@qq.com>
2018-09-04 14:54:40 +0800

    激活账号设置

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -9,34 +9,22 @@ class UserController
     public function register(){
         view('users.add');
     }
-    public function hello()
-    {
-        // 取数据
-        $user = new User;
-        $name = $user->getName();
-
-        // 加载视图
-        view('users.hello', [
-            'name' => $name
-        ]);
-    }
-
-    public function world()
-    {
-        echo 'world';
-    }
-
     public function store(){
         $email = $_POST['email'];
         $password = md5($_POST['password']);
 
-        // 2. 插入到数据库中
-        $user = new User;
-        $ret = $user->add($email, $password);
-        if(!$ret)
-        {
-            die('注册失败！');
-        }
+        // 生成激活码
+        $code = md5(rand(1,99999));
+        $redis = \libs\Redis::gitInstance();
+        // 序列化
+        $values = json_encode([
+            'email'=>$email,
+            'password'=>$password,
+        ]);
+        $key = "temp_user:{$code}";
+        $redis->setex($key,300,$values);
+
+
         // 从邮箱地址中取出姓名     
         $name = explode('@',$email);
          // 构造收件人地址
@@ -45,7 +33,9 @@ class UserController
         // 构造信息数组
         $message = [
             'title'=>'欢迎加入全栈一班',
-            'content'=>"点击以下链接进行激活：<br> <a href=''>点击激活</a>。",
+            'content'=>"点击以下链接进行激活：\r\n 
+            <a href='http://localhost:9999/user/active_user?code={$code}'>
+            http://localhost:9999/user/active_user?code={$code}</a>\r\n如果不能点击，请复制地址",
             'from'=>$from,
         ];
          // 把消息转成字符串(JSON ==> 序列化)
@@ -55,4 +45,32 @@ class UserController
         $redis->lpush('email',$message);
         echo "ok";
     }
+
+    public function active_user(){
+        $code = $_GET['code'];
+        $redis = \libs\redis::gitInstance();
+        $key = 'temp_user:'.$code;
+        $data = $redis->get($key);
+        if($data){
+            $redis->del($key);
+            $data = json_decode($data,true);
+
+            $user = new User;
+            $user->add($data['email'],$data['password']);
+            // die("激活成功！");
+            header('Location:/user/login');
+
+        }
+        else
+        {
+            die('激活码无效！');
+        }
+    }
+
+
+
+
+
+
+
 }
